180a12fa0b6a4908283bbe832392793032750f77
吉翔 <jx4554@gmail.com>
2018-07-14 10:20:07 +0800

    Separating Produciton and Development Configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
-const path = require('path');
+// const path = require('path');
 
-module.exports = {
-  entry: './js/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: process.env.NODE_ENV == "production" ? '[chunkhash]-bundle.js' : "bundle.js"
+module.exports = function(env) {
+  if (env === undefined) {
+    env = "dev"
   }
+  return require(`./webpack/${env}.js`)
 }
