f21379e52a710e4a14bf59bd04a49ee4844c76d3
吉翔 <jx4554@gmail.com>
2018-07-14 10:05:38 +0800

    config production hash bundle js

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,6 @@ module.exports = {
   entry: './js/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: process.env.NODE_ENV == "production" ? '[chunkhash]-bundle.js' : "bundle.js"
   }
 }
