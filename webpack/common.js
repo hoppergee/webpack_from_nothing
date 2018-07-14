a3a0d9720c905d88fab3befd56d816b55a6bb487
吉翔 <jx4554@gmail.com>
2018-07-14 18:40:31 +0800

    User css-loader to import and laod css files.

--- webpack/common.js
+++ webpack/common.js
@@ -1,6 +1,14 @@
 const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /.css$/,
+        use: 'css-loader'
+      }
+    ]
+  },
   plugins: [
     new HtmlPlugin({
       template: "./html/index.html"
