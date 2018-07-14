22b80b4a387452fdf61ece868316eec85e9a1360
吉翔 <jx4554@gmail.com>
2018-07-14 10:45:34 +0800

    Accessing the Hashed Filename in our HTML

--- webpack/common.js
+++ webpack/common.js
@@ -1,3 +1,10 @@
+const HtmlPlugin = require('html-webpack-plugin')
+
 module.exports = {
+  plugins: [
+    new HtmlPlugin({
+      templates: "./html/index.html"
+    })
+  ],
   entry: './js/index.js'
 };
